d53f7d37a61ccbce16f4fdd1a5fb03e080da3b9b
angry_hei <angrh_hei@hello.com>
2023-02-19 12:21:28 +0800

    打开video的持久化日志

--- cmd/video/main.go
+++ cmd/video/main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"net"
+
 	"github.com/cloudwego/kitex/pkg/klog"
 	"github.com/cloudwego/kitex/pkg/limit"
 	"github.com/cloudwego/kitex/pkg/rpcinfo"
 	"github.com/cloudwego/kitex/server"
+	"github.com/kitex-contrib/obs-opentelemetry/tracing"
 	etcd "github.com/kitex-contrib/registry-etcd"
 	"github.com/xiaohei366/TinyTiktok/cmd/video/initialize"
 	"github.com/xiaohei366/TinyTiktok/kitex_gen/VideoServer/videosrv"
+	mw "github.com/xiaohei366/TinyTiktok/pkg/middleware"
 	"github.com/xiaohei366/TinyTiktok/pkg/shared"
-	"net"
 )
 
 func main() {
@@ -31,9 +34,9 @@ func main() {
 		server.WithRegistry(r),
 		server.WithLimit(&limit.Option{MaxConnections: 1000, MaxQPS: 100}),
 		server.WithMuxTransport(),
-		//server.WithMiddleware(mw.CommonMiddleware),
-		//server.WithMiddleware(mw.ServerMiddleware),
-		//server.WithSuite(tracing.NewServerSuite()),
+		server.WithMiddleware(mw.CommonMiddleware),
+		server.WithMiddleware(mw.ServerMiddleware),
+		server.WithSuite(tracing.NewServerSuite()),
 		server.WithServerBasicInfo(&rpcinfo.EndpointBasicInfo{ServiceName: shared.PublishServiceName}),
 	)
 	//run video server
